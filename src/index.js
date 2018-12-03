4704ff5e3eb5c670ec68882ba983b7f5a2f6ff69
coreytegeler <coreytegeler@gmail.com>
2018-12-03 11:24:36 -0500

    uses oembed

--- src/index.js
+++ src/index.js
@@ -13,7 +13,8 @@ class FourCorners {
 	}
 
 	init(userOpts) {
-		window.FOURCORNERS = [];
+		let proto = this;
+		proto.embeds = [];
 		const defaultOpts = {
 			selector: '.fc-embed:not(.fc-init)',
 			cornerStroke: '6px',
@@ -28,9 +29,9 @@ class FourCorners {
 		const embeds = Array.from(document.querySelectorAll(opts.selector));
 		embeds.forEach(function(embed, i) {
 			const inst = new FourCorners(embed, opts);
-			window.FOURCORNERS.push(inst);
+			proto.embeds.push(inst);
 		});
-		return window.FOURCORNERS;
+		return proto.embeds;
 	}
 
 	openCorner(slug) {
@@ -77,13 +78,30 @@ const initEmbed = (inst) => {
 }
 
 const addPhoto = (inst)  => {
-	if(!inst.data){return}
-	const imgSrc = inst.data.img;
+	let embed = inst.elems.embed;
 	let img = document.createElement('img');
-	img.classList.add('fc-photo');
-	img.src = imgSrc;
-	inst.elems.embed.appendChild(img);
-	return img;
+	img.classList.add('fc-img');
+	let photo = '';
+	const photoSelector = '.fc-photo';
+	if(!embed.querySelector(photoSelector)) {
+		photo = document.createElement('div');
+		photo.classList.add('fc-photo');
+		const pseudoImg = new Image();
+		const src = inst.data.img;
+		pseudoImg.onload = (e) => {
+			img.src = src;
+			photo.classList.add('fc-loaded');
+		}
+		pseudoImg.onerror = (e) => {
+			console.log(e);
+		}
+		pseudoImg.src = src;
+		photo.appendChild(img);
+	} else {
+		photo = embed.querySelector(photoSelector);
+	}
+	embed.appendChild(photo);
+	return photo;
 }
 
 const addPanels = (inst) => {
@@ -107,7 +125,7 @@ const addPanels = (inst) => {
 				Object.entries(data).forEach(([prop, val]) => {
 					if(!val){return}
 					let row = document.createElement('div');
-					row.className = 'fc-row';
+					row.classList.add('fc-row', 'fc-'+prop);
 					if(!['media','links'].includes(prop)) {
 						let label = document.createElement('div');
 						label.className = 'fc-label';
@@ -145,13 +163,12 @@ const addMedia = (arr) => {
 			let img = document.createElement('img');
 			img.src = obj.url;
 			subRow.appendChild(img);
-		} else if(obj.type == 'youtube') {
-			let iframe = '<iframe src="'+obj.url+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-			subRow.innerHTML = iframe;
+		} else {
+			getMediaEmbed(obj, subRow)
 		}
 		if(obj.credit) {
 			let credit = document.createElement('div');
-			credit.className = 'fc-credit';
+			credit.className = 'fc-sub-credit';
 			credit.innerHTML = obj.credit;
 			subRow.appendChild(credit);
 		}
@@ -159,6 +176,45 @@ const addMedia = (arr) => {
 	});
 	return subRows;
 }
+
+const getMediaEmbed = (obj, subRow) => {
+	let req = '';
+	switch(obj.type) {
+		case 'youtube':
+			req = 'https://www.youtube.com/oembed?url='+obj.url;
+			break;
+		case 'vimeo':
+			req = 'https://vimeo.com/api/oembed.json?url='+obj.url;
+			break;
+		case 'soundcloud':
+			req = 'https://soundcloud.com/oembed?format=json&url='+obj.url;
+			break;
+		default:
+			return false;
+			break;
+	}
+	fetch(req)
+		.then(res => {
+			if (!res.ok) {throw Error(res.statusText)}
+			return res.json();
+		})
+		.then(res => {
+			var mediaWrap = document.createElement('div');
+			mediaWrap.className = 'fc-media-wrap';
+			mediaWrap.innerHTML =  res.html;
+			if(Number.isInteger(res.width,res.height)) {
+				const ratio = res.height/res.width;
+				mediaWrap.classList.add('fc-responsive')
+				mediaWrap.style.paddingBottom = (ratio*100)+'%';
+			}
+			subRow.prepend(mediaWrap);
+		})
+		.catch(function(err) {
+			console.log(err);
+		});
+}
+
+	
 
 const addLinks = (arr) => {
 	let subRows = document.createElement('div');
@@ -190,21 +246,27 @@ const addCorners = (inst) => {
 	let corners = {};
 	let embed = inst.elems.embed;
 	let photo = inst.elems.photo;
-	// let cornerStroke = inst.opts.cornerStroke;
-	// let cornerSize = inst.opts.cornerSize;
-	// let cornerMargin = inst.opts.cornerMargin;
+
+	embed.addEventListener('mouseenter', function(e) {
+		hoverEmbed(e, inst);
+	});
+	embed.addEventListener('mouseleave', function(e) {
+		unhoverEmbed(e, inst);
+	});
+
+	if(photo) {
+		photo.addEventListener('click', function(e) {
+			clickPhoto(e, inst);
+		});
+	}
+
 	inst.corners.forEach(function(slug, i) {
 		const cornerSelector = '.fc-corner[data-slug="'+slug+'"]';
 		if(embed.querySelector(cornerSelector)) {return;}
 		let corner = document.createElement('div');
+		if(!corner) {return;}
 		corner.classList.add('fc-corner');
 		corner.dataset.slug = slug;
-		embed.addEventListener('mouseenter', function(e) {
-			hoverEmbed(e, inst);
-		});
-		embed.addEventListener('mouseleave', function(e) {
-			unhoverEmbed(e, inst);
-		});
 		corner.addEventListener('mouseenter', function(e) {
 			hoverCorner(e, inst);
 		});
@@ -213,9 +275,6 @@ const addCorners = (inst) => {
 		});
 		corner.addEventListener('click', function(e) {
 			clickCorner(e, inst);
-		});
-		photo.addEventListener('click', function(e) {
-			clickPhoto(e, inst);
 		});
 		corners[slug] = corner;
 		embed.appendChild(corner);
@@ -272,24 +331,23 @@ const clickPhoto = (e, inst) => {
 	inst.closeCorner();
 }
 
-const addStyles = (elem, styles) => {
-	Object.entries(styles).forEach(([prop, val]) => {
-		elem.style[prop] = val;
-	});
-	return elem;
-}
-
+// const addStyles = (elem, styles) => {
+// 	Object.entries(styles).forEach(([prop, val]) => {
+// 		elem.style[prop] = val;
+// 	});
+// 	return elem;
+// }
 
 //Adds namespace to all classes
-const fc = (input) => {
-	const ns = 'fc';
-	let output = [];
-	if(!Array.isArray(input)){input = [input];}
-	input.forEach(function(str, i) {
-		output[i] = ns+'_'+str;
-	});
-	return output;
-}
+// const fc = (input) => {
+// 	const ns = 'fc';
+// 	let output = [];
+// 	if(!Array.isArray(input)){input = [input];}
+// 	input.forEach(function(str, i) {
+// 		output[i] = ns+'_'+str;
+// 	});
+// 	return output;
+// }
 
 var wrapUrls = function (str) {
 	var urlPattern = /(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}\-\x{ffff}0-9]+-?)*[a-z\x{00a1}\-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}\-\x{ffff}]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?/ig;
